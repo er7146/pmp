9272cf3ab9420747383355443b89596e9aef1e41
Eddie <eddie@Eddies-MacBook-Pro.local>
2015-02-05 01:04:10 -0700

    update-3

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable #:validatable
          
-    has_many :orders ,
-    class_name: "Orders", foreign_key: "user_id"
-  has_many :order_type ,
-    class_name: "orders", foreign_key: "type"
+
 
   validate :name ,   presence: true, uniqueness: true
   #validate :email ,   presence: true, uniqueness: true
